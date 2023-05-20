b4a2b09e8b95f0a3581a54b5bb67ec3395f657b0
alsiesta <as@daslizenzatelier.de>
2023-05-20 10:15:17 +0200

    added sign-up form and its validation

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+export function passwordsMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    if (password && confirmPassword && password !== confirmPassword) {
+      return { passwordsDontMatch: true}
+    } 
+    return null;
+  };
+}
+
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +19,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  signUpForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  }, { validators: passwordsMatchValidator()});
 
+  get name() { return this.signUpForm.get('name') };
+  get email() { return this.signUpForm.get('email') };
+  get password() { return this.signUpForm.get('password') };
+  get confirmPassword() { return this.signUpForm.get('confirmPassword') };
+
+  submit(){}
 }
